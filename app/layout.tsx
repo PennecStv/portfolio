c3ab81b7195790f5b083b99b484fdcaa8053c0e7
Matteo ANDRIATSILAVO <matteoandria@gmail.com>
2026-04-24 00:01:05 +0200

    [ADD] Append the footer to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import { ThemeProvider } from "next-themes";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,9 @@ export default function RootLayout({
           enableSystem={false}
         >
           {children}
+
+          <Footer />
+
         </ThemeProvider>
       </body>
     </html>
